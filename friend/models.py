7c717ad7db00c75066628969f4cddb65ba5af994
hongmon1 <hongmon1@naver.com>
2021-09-21 22:49:59 +0900

    친구추가 기능

--- friend/models.py
+++ friend/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+from login_signup.models import User
 
 # Create your models here.
 
 class user_friend(models.Model):
 
-    user_id = models.AutoField(primary_key=True)
-    friend_id = models.IntegerField()
+    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='user_id', null=False, default = False)
+    friend_id = models.CharField(max_length=64, unique=True, null=False, default=False)
 
     class Meta :
         db_table = 'user_friend'
--- friend/views.py
+++ friend/views.py
@@ -1,13 +1,59 @@
-from django.shortcuts import render
+from MySQLdb import IntegrityError
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import user_friend
+from login_signup.models import User
 from django.http import JsonResponse
-from django.contrib.auth.hashers import make_password, check_password
 
 # Create your views here.
 
-class RelationCreateView(APIView):
+class AddFriend(APIView):
     def post(self, request):
 
-        user_i = request.data.get('user')
+        user_id = request.data.get('user_id')
+        add_friend_id = request.data.get('add_friend_id')
+
+
+        # 유저 아이디가 올바른지
+        try:
+            user = User.objects.filter(user_id=user_id)
+        except User.DoesNotExist:
+            return self.response(message = '잘못된 요청입니다.', status=200)
+
+
+        # 친구 추가할 ID가 User 테이블에 존재하는지
+        try:
+            User.objects.filter(user_id=add_friend_id)
+        except User.DoesNotExist:
+            return self.response(message = '잘못된 요청입니다.', status=200)
+
+        # 친구 추가 하기
+
+        # 친구 추가 아이디가 본인 아이디 와 같으면
+        if user_id == add_friend_id:
+            raise IntegrityError
+
+        # 원래 친구가 아니면
+        friend = user_friend.objects.filter(user_id=user_id, friend_id=add_friend_id)
+        if friend is not None:
+            user_friend.objects.create(user_id=user.user_id, friend_id=add_friend_id)
+
+        data = dict(
+            user_id = user_id,
+            friend_id = add_friend_id
+        )
+
+        return Response(data)
+
+
+class ShowFriend(APIView):
+    def post(self, request):
+
+        user_id = request.data.get('user_id')
+
+
+        data = dict(
+            user_id = user_id
+        )
+
+        return Response(data)
