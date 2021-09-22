08063447f0fd1203af7c7e5a0fb154113f4f0a2e
hongmon1 <hongmon1@naver.com>
2021-09-22 21:13:08 +0900

    friend 재생성

--- friend/models.py
+++ friend/models.py
@@ -5,8 +5,8 @@ from login_signup.models import User
 
 class user_friend(models.Model):
 
-    f_user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=False, default = False)
-    f_friend_id = models.CharField(max_length=255, unique=True, null=False, default=False)
+    uf_user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=False, default = False)
+    uf_friend_id = models.CharField(max_length=255, unique=True, null=False, default=False)
 
     class Meta :
         db_table = 'user_friend'
